3b3e0457f9abc766b987e0b7554bfa1b53165d59
maxiar0s <maxi.aros.osorio@gmail.com>
2026-03-02 13:58:25 -0300

    feat: modifying docker compose and adding .env example

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.get('/api/health', (_req, res) => {
   res.status(200).json({ status: 'ok', timestamp: new Date().toISOString() });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`API Control Horas corriendo en puerto ${PORT}`);
 });
